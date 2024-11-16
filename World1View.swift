9033555a9e8e10d7a0ad0aa516ac832e9bf6bdeb
samuelngyh <samuelngyh@gmail.com>
2024-11-16 10:42:21 +0800

    Update World1View.swift
    Added limited functionality to the Map

--- World1View.swift
+++ World1View.swift
@@ -1,5 +1,5 @@
 //
-//  TutorialView.swift
+//  World1View.swift
 //  Challenge 3 Application
 //
 //  Created by T Krobot on 11/11/R6.
@@ -16,35 +16,41 @@
 import SwiftUI
 
 struct World1View: View {
+    @State var nextView = ""
     var body: some View {
-        ZStack{
-            Text("World 1")
-                .font(.headline)
-                .frame(width: 300, height: 300, alignment: .top)
-            Image(systemName: "house")
-                .frame(width: 400, height: 100, alignment: .topLeading)
-                .onTapGesture{
-                     //HouseView
-                }
-            Image(systemName: "tree.fill")
-                .frame(width: 300, height: 70, alignment: .bottomTrailing)
-                .onTapGesture{
-                     //TreeView
-                }
-            
-            Image(systemName: "apple.image.playground")
-                .frame(width: 200, height: 40, alignment: .bottomLeading)
-                .onTapGesture{
-                     //WaterView
-                }
-            Image(systemName: "directcurrent")
-                .frame(width: 0, height: 60, alignment: .top)
-                .onTapGesture{
-                     //PlaygroundView
-                }
+        if nextView.isEmpty{
+            ZStack{
+                Text("World 1")
+                    .font(.headline)
+                    .frame(width: 300, height: 300, alignment: .top)
+                Image(systemName: "house")
+                    .frame(width: 400, height: 100, alignment: .topLeading)
+                    .onTapGesture{
+                        //HouseView
+                    }
+                Image(systemName: "tree.fill")
+                    .frame(width: 300, height: 70, alignment: .bottomTrailing)
+                    .onTapGesture{
+                        //TreeView
+                    }
+                
+                Image(systemName: "apple.image.playground")
+                    .frame(width: 200, height: 40, alignment: .bottomLeading)
+                    .onTapGesture{
+                        //PlaygroundView
+                    }
+                Image(systemName: "directcurrent")
+                    .frame(width: 0, height: 60, alignment: .top)
+                    .onTapGesture{
+                        nextView = "DuckView"
+                    }
+            }
+        }
+        if nextView == "DuckView"{
+            DuckView(duckScenario: ScenarioLibrary.duckScenario)
         }
     }
 }
 #Preview {
-    World1View()
+    World1View(nextView: "")
 }
