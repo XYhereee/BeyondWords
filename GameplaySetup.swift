eba401e306520dafdf8a46b1ef5b662ea9a34929
samuelngyh <samuelngyh@gmail.com>
2024-11-16 09:59:28 +0800

    Updated GameplaySetup.swift
    Cleaned up a bunch of code

--- GameplaySetup.swift
+++ GameplaySetup.swift
@@ -8,20 +8,23 @@
 import Foundation
 import SwiftUI
 
-struct Scenario: Identifiable{
-    let image: String?
+
+class ScenarioClass: Identifiable{
+    let title: String
+    let image: String
     let imageCoordinates: Array<Int>
     var imagePosition: Int
     var options: Array<String>
     var id: String
-}
-
-class ScenarioClass{
-    var sampleScenario:Scenario
-    init(image: String? = nil, imageCoordinates: Array<Int> = [], imagePosition: Int, options: Array<String> = [], id: String = ""){
-        sampleScenario = Scenario(image: "", imageCoordinates: [], imagePosition: 0, options: [], id: "")
+    init(title: String, image: String, imageCoordinates: Array<Int>, imagePosition: Int, options: Array<String>, id: String = ""){
+        self.title = title
+        self.image = image
+        self.imageCoordinates = imageCoordinates
+        self.imagePosition = imagePosition
+        self.options = options
+        self.id = id
     }
 }
 struct ScenarioLibrary{
-    var duckScenario: ScenarioClass = .init(image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenario")
+    var duckScenario: ScenarioClass = .init(title: "You see a duck walking around the perimeter of the pond.", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenario")
 }
