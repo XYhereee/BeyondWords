f143e0f8d19d82e07c10a6a8c4fdd8ce6e14fcc2
samuelngyh <samuelngyh@gmail.com>
2024-11-17 22:23:35 +0800

    1.2 GameplayStageView.swift
    Add functionality to track which button is pressed in an array alongside GameplayButtonView

--- GameplayStageView.swift
+++ GameplayStageView.swift
@@ -9,20 +9,32 @@ import SwiftUI
 
 struct GameplayStageView: View {
     @Binding var situation: ScenarioClass
+    @Binding var progression: [Int]
     var body: some View {
-    
         VStack {
+            Text("\(situation.title)")
+                .font(.title)
+            Text("\(situation.innate)")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding()
             HStack {
-                ForEach(situation.options, id: \.self) { option in
-                    GameplayButtonView(content: option, situation: $situation)
+                ForEach(Array(situation.options.enumerated()), id: \.element) { option, content in
+                    GameplayButtonView(content: content, progression: $progression, optionNumber: option + 1)
+
                 }
+            }
+            .onChange(of: situation.options) { oldValue, newValue in
+                print(newValue)
             }
         }
     }
 }
 
 #Preview {
-    @Previewable @State var situation: ScenarioClass = .init(title: "placeholder value", image: "stuff", imageCoordinates: [0, 0], imagePosition: 0, options: ["Option 1", "Option 2"], id: "12345")
-    GameplayStageView(situation: $situation)
+    @Previewable @State var situation: ScenarioClass = .init(title: "You Witness A Duck", innate: "You see a duck walking around the perimeter of the pond. You hardly see ducks around here during your numerous visits here. Your curiosity is piqued.", speech:"", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenarioOpening")
+    @Previewable @State var progression: [Int] = []
+    GameplayStageView(situation: $situation, progression: $progression)
 }
+
 
