2d24c2e862f66d4e11fd7fbe2dc22a4a385acbd9
samuelngyh <samuelngyh@gmail.com>
2024-11-17 22:22:13 +0800

    Updated 1.2 GameplaySetup.swift
    Added a few more pieces of dialogue, will add more in future.

--- GameplaySetup.swift
+++ GameplaySetup.swift
@@ -1,4 +1,4 @@
-//
+/
 //  GameplaySetup.swift
 //  Challenge 3 Application
 //
@@ -9,22 +9,18 @@ import Foundation
 import SwiftUI
 
 
-class ScenarioClass: Identifiable{
+struct ScenarioClass: Identifiable {
     let title: String
+    let innate: String
+    let speech: String
     let image: String
     let imageCoordinates: Array<Int>
     var imagePosition: Int
     var options: Array<String>
     var id: String
-    init(title: String, image: String, imageCoordinates: Array<Int>, imagePosition: Int, options: Array<String>, id: String = ""){
-        self.title = title
-        self.image = image
-        self.imageCoordinates = imageCoordinates
-        self.imagePosition = imagePosition
-        self.options = options
-        self.id = id
-    }
 }
 struct ScenarioLibrary{
-    var duckScenario: ScenarioClass = .init(title: "You see a duck walking around the perimeter of the pond.", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenario")
+    static var duckScenarioOpening: ScenarioClass = .init(title: "You Witness A Duck", innate: "You see a duck walking around the perimeter of the pond. You hardly see ducks around here during your numerous visits here. Your curiosity is piqued.", speech:"", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenarioOpening")
+    static var duckScenarioChase: ScenarioClass = .init(title: "Chase that Duck", innate: "You chase the duck around the perimeter of the pond. Taking into consideration its speed, you're not sure if it's a duck or a human. After some time, the duck chooses to take refuge in the center of the pond.", speech: "Quack!", image: "player chase duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase duck into pond", "Stop chasing duck"], id: "duckScenarioChase")
+    static var duckScenarioWatch: ScenarioClass = .init(title: "Take a Break", innate: "", speech: "", image: "player chase duck", imageCoordinates: [100,100], imagePosition: 0, options: [], id: "duckScenarioWatch")
 }
