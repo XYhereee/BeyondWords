6e3642b6e5f1ca9ee7931cd2f681d7981ff9f503
samuelngyh <samuelngyh@gmail.com>
2024-11-17 22:25:18 +0800

    1.2 DuckView.swift

--- DuckView.swift
+++ DuckView.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
-
 struct DuckView: View {
-    @State var duckScenario: ScenarioClass
+    @State var progression: [Int] = []
+    @State var currentScenario: ScenarioClass = ScenarioLibrary.duckScenarioOpening
     var body: some View {
-        GameplayStageView(situation: $duckScenario)
+        GameplayStageView(situation: $currentScenario, progression: $progression)
+            .onChange(of: progression) { oldValue, newProgression in
+                if newProgression[0] == 1 {
+                    currentScenario = ScenarioLibrary.duckScenarioChase
+                }
+                if newProgression[0] == 2 {
+                    currentScenario = ScenarioLibrary.duckScenarioWatch
+                }
+            }
     }
 }
 
 #Preview {
-    @Previewable @State var duckScenario: ScenarioClass = .init(title: "You see a duck walking around the perimeter of the pond.", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenario")
-    DuckView(duckScenario: duckScenario)
+    @Previewable @State var duckScenario: ScenarioClass = .init(title: "You Witness A Duck", innate: "You see a duck walking around the perimeter of the pond. You hardly see ducks around here during your numerous visits here. Your curiosity is piqued.", speech:"", image:"duck", imageCoordinates: [100,100], imagePosition: 0, options: ["Chase Duck","Watch from a Distance"], id: "duckScenarioOpening")
+    DuckView(progression: [])
 }
